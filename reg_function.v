5d15f663c6ae476640622f287d9a104aafe71792
joe <musketeerdt@gmail.com>
2023-05-30 15:15:27 +0800

    for_simulate

--- reg_function.v
+++ reg_function.v
@@ -13,17 +13,100 @@ module reg_function(
 	input [1:0] res_dest,
 	input enact
 );
-always@(negedge clk)
+//always@(negedge clk)
+//	begin
+//		case(RA)
+//				2'b00:
+//				begin
+//					X<=R0;
+//					if(res_dest==2'b00&&!enact)
+//					begin
+//						R0<=res_alu;
+//					end
+//					else if(wr==0&&rd==1)
+//					begin
+//						R0<=DATA_INPUT;
+//					end
+//					else if(wr==1&&rd==1)
+//					begin
+//						R0<=res_alu;
+//					end
+//				end
+//				2'b01:
+//				begin
+//					X<=R1;
+//					if(res_dest==2'b01&&!enact)
+//					begin
+//						R1<=res_alu;
+//					end
+//					else if(wr==0&&rd==1)
+//					begin
+//						R1<=DATA_INPUT;
+//					end
+//					else if(wr==1&&rd==1)
+//					begin
+//						R1<=res_alu;
+//					end
+//				end
+//				2'b10:
+//				begin
+//					X<=R2;
+//					if(res_dest==2'b10&&!enact)
+//					begin
+//						R2<=res_alu;
+//					end
+//					else if(wr==0&&rd==1)
+//					begin
+//						R2<=DATA_INPUT;
+//					end
+//					else if(wr==1&&rd==1)
+//					begin
+//						R2<=res_alu;
+//					end
+//				end
+//				2'b11:
+//				begin
+//					X<=R3;
+//					if(res_dest==2'b11&&!enact)
+//					begin
+//						R3<=res_alu;
+//					end
+//					else if(wr==0&&rd==1)
+//					begin
+//						R3<=DATA_INPUT;
+//					end
+//					else if(wr==1&&rd==1)
+//					begin
+//						R3<=res_alu;
+//					end
+//				end
+//			endcase
+//	end
+	always@(posedge clk)
 	begin
-		case(RA)
+		if(res_dest==2'b00&&!enact)
+		begin
+			R0<=res_alu;
+		end
+		else if(res_dest==2'b01&&!enact)
+		begin
+			R1<=res_alu;
+		end
+		else if(res_dest==2'b10&&!enact)
+		begin
+			R2<=res_alu;
+		end
+		else if(res_dest==2'b11&&!enact)
+		begin
+			R3<=res_alu;
+		end
+		else 
+		begin
+			case(RA)
 				2'b00:
 				begin
 					X<=R0;
-					if(res_dest==2'b00&&!enact)
-					begin
-						R0<=res_alu;
-					end
-					else if(wr==0&&rd==1)
+					if(wr==0&&rd==1)
 					begin
 						R0<=DATA_INPUT;
 					end
@@ -35,11 +118,7 @@ always@(negedge clk)
 				2'b01:
 				begin
 					X<=R1;
-					if(res_dest==2'b01&&!enact)
-					begin
-						R1<=res_alu;
-					end
-					else if(wr==0&&rd==1)
+					if(wr==0&&rd==1)
 					begin
 						R1<=DATA_INPUT;
 					end
@@ -51,11 +130,7 @@ always@(negedge clk)
 				2'b10:
 				begin
 					X<=R2;
-					if(res_dest==2'b10&&!enact)
-					begin
-						R2<=res_alu;
-					end
-					else if(wr==0&&rd==1)
+					if(wr==0&&rd==1)
 					begin
 						R2<=DATA_INPUT;
 					end
@@ -67,11 +142,7 @@ always@(negedge clk)
 				2'b11:
 				begin
 					X<=R3;
-					if(res_dest==2'b11&&!enact)
-					begin
-						R3<=res_alu;
-					end
-					else if(wr==0&&rd==1)
+					if(wr==0&&rd==1)
 					begin
 						R3<=DATA_INPUT;
 					end
@@ -81,6 +152,7 @@ always@(negedge clk)
 					end
 				end
 			endcase
+		end
 	end
 	endmodule
 	 
